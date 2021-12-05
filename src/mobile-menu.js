31c681ed14857dd53404251ac7b94cb5a7a0f78d
Alexander Pikurin <pikchas95@gmail.com>
2021-12-05 15:57:51 +0200

    добавил скрипт менюшки

---
+++ src/mobile-menu.js
@@ -0,0 +1,19 @@
+(() => {
+  const mobileMenu = document.querySelector('[data-menu]');
+  const openMenuBtn = document.querySelector('[data-menu-open]');
+  const closeMenuBtn = document.querySelector('[data-menu-close]');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  
+})();
